2976654ecb8a3acea7ba6185d2546b7cc087aedf
Carlos Manuel Olivera Mispireta <correo-deseado@example.com>
2025-08-29 17:52:13 -0500

    fix CORS

--- src/helpers/response.ts
+++ src/helpers/response.ts
@@ -1,5 +1,10 @@
 export const response = (statusCode: number, body: object) => ({
   statusCode,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization",
+  },
   body: JSON.stringify(body),
 });

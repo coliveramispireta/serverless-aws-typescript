2f600ef49bd9b0c1101494743d63e55f024eb750
Carlos Manuel Olivera Mispireta <correo-deseado@example.com>
2026-01-30 19:23:55 -0500

    /

--- src/helpers/response.ts
+++ src/helpers/response.ts
@@ -2,7 +2,7 @@ export const response = (statusCode: number, body: object) => ({
   statusCode,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://d2exyey26zrqxx.cloudfront.net/",
+    "Access-Control-Allow-Origin": "https://d2exyey26zrqxx.cloudfront.net",
     "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE",
     "Access-Control-Allow-Headers": "Content-Type,Authorization",
   },
